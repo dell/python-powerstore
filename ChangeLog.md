be46f74bf88282992791dbf15262a0a599805f52
Shendge <Akash.Shendge@emc.com>
2021-06-25 13:12:48 +0530

    RTS 1.3.0

---
+++ ChangeLog.md
@@ -0,0 +1,9 @@
+# PyPowerStore Change Log
+## Version 1.3.0 - released on 25/06/21
+- Added replication functionalities includes managing replication rule, replication session and getting high level facts about all these entities.
+
+## Version 1.2.0 - released on 25/09/20
+- Added file provisioning operations includes managing filesystem, filesystem snapshot, SMB shares, NFS exports, quotas and getting high level facts about all these entities.
+
+## Version 1.1.0 - released on 06/05/20
+- Added block provisioning operations includes managing volume, volume group, host, host group, snapshot rule, protection policy, snapshots and getting high level facts about all these entities.
--- ProgrammersGuideExamples/protection_examples.py
+++ ProgrammersGuideExamples/protection_examples.py
@@ -44,20 +44,46 @@ print(RESP)
 # Delete a snapshot rule
 CONN.protection.delete_snapshot_rule(snapshot_rule_id=CREATED_SRULE_ID)
 
+# Create replication rule
+rule_id = CONN.protection.create_replication_rule(
+    name="test-rule", rpo="Thirty_Minutes",
+    remote_system_id="8b3421f6-7a0e-4211-825c-dc6e869abcc4")
+print(rule_id)
+
+# Get replication rule details by id
+resp = CONN.protection.get_replication_rule_details(
+    replication_rule_id=rule_id['id'])
+
+# Get replication rule details by name
+resp = CONN.protection.get_replication_rule_by_name(name="test-rule")
+
+# Modify replication rule
+resp = CONN.protection.modify_replication_rule(
+    replication_rule_id=rule_id['id'], name="test-rule-modified", rpo="One_Hour")
+
+# Delete replication rule
+resp = CONN.protection.delete_replication_rule(
+    replication_rule_id=rule_id['id'])
+
 # Protection policy examples
 SRULE_IDS = []
 for index in range(1, 4):
     srule_id = CONN.protection.create_snapshot_rule_by_interval(
-        name="pr-sdk-srule-{}".format(index), desired_retention=1,
-        interval="Five_Minutes").get('id')
+        name="pr-sdk-srule-{}".format(index), desired_retention=3,
+        interval="Two_Hours").get('id')
     if srule_id:
         SRULE_IDS.append(srule_id)
+
+# Create replication rule
+rule_id = CONN.protection.create_replication_rule(
+    name="test-rule", rpo="Thirty_Minutes",
+    remote_system_id="8b3421f6-7a0e-4211-825c-dc6e869abcc4")
+
 # Create a protection policy
 RESP = CONN.protection.create_protection_policy(
     name="pr-sdk-prot-pol",
     description="protection policy for testing pypowerstore",
-    snapshot_rule_ids=SRULE_IDS[:1]
-)
+    snapshot_rule_ids=SRULE_IDS[:1])
 CREATED_PP_ID = RESP.get("id")
 print(RESP)
 
@@ -85,7 +111,20 @@ RESP = CONN.protection.remove_snapshot_rules_from_protection_policy(
 )
 print(RESP)
 
+# Add replication rule to the protection policy
+resp = CONN.protection.add_replication_rules_to_protection_policy(
+    policy_id=CREATED_PP_ID, add_replication_rule_ids=[rule_id['id']])
+print(resp)
+
+# Remove replication rule from the protection policy
+resp = CONN.protection.remove_replication_rules_from_protection_policy(
+    policy_id=CREATED_PP_ID, remove_replication_rule_ids=[rule_id['id']])
+print(resp)
+
 # Delete a protection policy
 CONN.protection.delete_protection_policy(policy_id=CREATED_PP_ID)
 for srule_id in SRULE_IDS:
     CONN.protection.delete_snapshot_rule(snapshot_rule_id=srule_id)
+
+# Delete replication rule
+CONN.protection.delete_replication_rule(replication_rule_id=rule_id['id'])
