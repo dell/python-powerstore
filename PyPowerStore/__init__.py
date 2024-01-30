4c4a5d3e417d4069cc3f9ab758ebc697402ec420
sharmb5 <bhavneet.sharma@dell.com>
2024-01-30 08:01:47 -0500

    Updating version in init file

--- PyPowerStore/__init__.py
+++ PyPowerStore/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+# Copyright: (c) 2024, Dell Technologies
 """__init__.py."""
 
 __title__ = 'PyPowerStore'
-__version__ = '2.1.0.0'
+__version__ = '3.0.0.0'
 __author__ = 'Dell Technologies or its subsidiaries'
 __copyright__ = 'Copyright 2019 Dell Technologies'
