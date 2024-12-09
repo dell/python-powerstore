a4d45ccc5e270b99847a209f035bef0161932bde
Datta <trisha_datta@dell.com>
2024-12-09 11:20:17 +0530

    Updating the SDK version

--- PyPowerStore/__init__.py
+++ PyPowerStore/__init__.py
@@ -3,6 +3,6 @@
 """__init__.py."""
 
 __title__ = 'PyPowerStore'
-__version__ = '3.3.0.0'
+__version__ = '3.4.0.0'
 __author__ = 'Dell Technologies or its subsidiaries'
 __copyright__ = 'Copyright 2024 Dell Technologies'
