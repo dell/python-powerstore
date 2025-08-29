7eeabb2c6daf7ffc5f2d94a9a7f90609fbb2e74d
martip5 <paule.martin@dell.com>
2025-08-29 10:53:29 +0100

    Added metrics.py

---
+++ ProgrammersGuideExamples/metrics_examples.py
@@ -0,0 +1,20 @@
+# Copyright: (c) 2024, Dell Technologies
+
+"""Local User Operations"""
+
+# pylint: disable=invalid-name,assignment-from-none,duplicate-code
+
+from PyPowerStore import powerstore_conn
+
+CONN = powerstore_conn.PowerStoreConn(
+    username="<username>",
+    password="<password>",
+    server_ip="<IP>",
+    verify=False,
+    application_type="<Application>",
+    timeout=180.0,
+)
+
+pprint ((CONN.metrics.get_performance_metrics(
+    entity="performance_metrics_by_appliance", entity_id="A1",
+                                     interval="Five_Sec")))
