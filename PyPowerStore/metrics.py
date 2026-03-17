20286ef843248c48566f93d09940313f87eb7b85
rounak-adhikary <Rounak.Adhikary@dell.com>
2026-03-17 16:45:19 +0530

    Fix: Add missing comma (#59)
    * Fix: Add missing comma
    
    * Unit tests and lint fixes

--- PyPowerStore/metrics.py
+++ PyPowerStore/metrics.py
@@ -1,5 +1,4 @@
-
-from collections import namedtuple
+"""PowerStore metrics module for performance monitoring."""
 
 from PyPowerStore.utils import constants, helpers
 
@@ -7,9 +6,13 @@ LOG = helpers.get_logger(__name__)
 
 
 class MetricsFunctions:
-    """Metric related functionality for PowerStore."""
+    """Metric related functionality for PowerStore.
+
+    This class provides methods to retrieve performance metrics
+    from PowerStore appliances.
+    """
     def __init__(self, provisioning, enable_log=False):
-        """Initializes ProtectionFunctions Class
+        """Initializes MetricsFunctions Class
 
         :param provisioning: Provisioning class object
         :type provisioning: Provisioning
@@ -19,20 +22,24 @@ class MetricsFunctions:
         self.provisioning = provisioning
         self.server_ip = provisioning.server_ip
         self.rest_client = provisioning.client
-        global LOG # Reset LOG based on param
-        LOG = helpers.get_logger(__name__, enable_log=enable_log)
+        self._enable_log = enable_log
+        self._log = helpers.get_logger(__name__, enable_log=enable_log)
 
 
     def get_performance_metrics(self, entity, entity_id, interval):
         """Get performance metrics for the given entity.
 
-        entity: The entity to get metrics for see documentation for Entity
+        :param entity: The entity to get metrics for see documentation for Entity
         class https://developer.dell.com/apis/3898/versions/{
         version}/reference/openapi.json/paths/~1metrics~1generate/post
-        entity_id: The ID of the entity - str
-        entity_id: The ID of the entity - str
-        interval: The interval to get metrics for, Best_Available
-                  Five_Sec, Twenty_Sec, Five_Mins, One_Hour, One_Day - str
+        :type entity: str
+        :param entity_id: The ID of the entity
+        :type entity_id: str
+        :param interval: The interval to get metrics for, Best_Available
+                  Five_Sec, Twenty_Sec, Five_Mins, One_Hour, One_Day
+        :type interval: str
+        :return: Performance metrics data
+        :rtype: dict
         """
         payload = {
             "entity": entity,
@@ -40,8 +47,16 @@ class MetricsFunctions:
             "interval": interval
         }
         return self.rest_client.request(
-                   constants.POST,
-               constants.GET_PERFORMANCE_METRICS_URL.format(
-                       self.server_ip),
-               payload=payload
-               )
+            constants.POST,
+            constants.GET_PERFORMANCE_METRICS_URL.format(
+                self.server_ip),
+            payload=payload
+        )
+
+    def is_logging_enabled(self):
+        """Check if logging is enabled for this instance.
+
+        :return: True if logging is enabled, False otherwise
+        :rtype: bool
+        """
+        return self._enable_log
